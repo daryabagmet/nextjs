714f7261154e85414efaee391109cda8ebcbca6f
darya <darya@MBP-darya.Home>
2024-04-09 21:40:00 +0100

    init property site

---
+++ property-pulse/app/layout.jsx
@@ -0,0 +1,24 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer'
+import '@/assets/styles/globals.css';
+
+
+export const metadata = {
+  title: 'PropertyPulse | Find The Perfect Rental',
+  description: 'Find your dream rental property',
+  keywords: 'rental, fint rentals, find properties'
+};
+
+const MainLayout = ({children}) => {
+  return (
+    <html lang='en'>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
+
+export default MainLayout;
